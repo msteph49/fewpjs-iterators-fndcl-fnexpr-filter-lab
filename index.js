d9b132cf0fea007042f9b29b261b464fc897d83f
Michael Stephens <mdstephens26@yahoo.com>
2021-09-12 21:42:09 -0400

    commit tree 5f63b42da2a21dde9686693c6fb855013e189a65

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching (driversArray, driverString) {
+    return driversArray.filter(driver =>
+        driver.toLowerCase() === driverString.toLowerCase()
+        )
+}
+
+function fuzzyMatch (driversArray, driverString) {
+    return driversArray.filter(driver =>
+        driver[0] === driverString[0])
+}
+
+function matchName(driversArray, driverString) {
+    return driversArray.filter(driver =>
+        driver.name === driverString)
+}
